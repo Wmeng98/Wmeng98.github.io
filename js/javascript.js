d3849a0bde9085b0ec50576e51a569fc51b1f376
HobbitA <w5meng@edu.uwaterloo.ca>
2017-07-31 20:01:45 -0500

    new commit with fadein js-jquery file

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+
+$(document).ready(
+  function() {
+    $('#q1').fadeIn(6000);
+  });
+
+
+
+$(document).ready(
+  function() {
+    $('#q2').fadeIn(8000);
+  });
+
+
+
+$(document).ready(
+  function() {
+    $('#q3').fadeIn(10000);
+  });
+
+$(document).ready(
+  function() {
+    $('#profile').fadeIn(3000);
+  });
+
