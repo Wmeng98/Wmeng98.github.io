cb5f20fbc44b294362c43cfbdfe2220a54cca43c
HobbitA <w5meng@edu.uwaterloo.ca>
2017-11-18 14:03:13 -0500

    fontawesome icon and responsive navigation bar implementations

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,14 @@
+// Vanilla JS  for Responsie NavBar
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+//
+
 
 $(document).ready(
   function() {
